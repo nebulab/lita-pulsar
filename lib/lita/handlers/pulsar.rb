a7e2c200a153f1b393c0deaeec341d979a01a94a
Matteo Latini <mtylty@gmail.com>
2015-02-09 18:35:14 +0100

    adds basic deploy feature

--- lib/lita/handlers/pulsar.rb
+++ lib/lita/handlers/pulsar.rb
@@ -1,6 +1,30 @@
 module Lita
   module Handlers
     class Pulsar < Handler
+      config :conf_repo, required: true, default: nil
+
+      route(/deploy\s+(.+)\s+(.+)/i, :deploy, command: true, help:
+        { 'deploy APPLICATION STAGE' => 'Deploys APPLICATION on STAGE' })
+
+      def deploy(response)
+        args = cmd_args(response.args)
+
+        Bundler.with_clean_env do
+          spawn("eval `ssh-agent -s` && ssh-add && bundle exec pulsar #{cmd_opts} #{args}; kill $SSH_AGENT_PID")
+        end
+
+        response.reply 'OK, I will run a deploy soon...'
+      end
+
+      private
+
+      def cmd_args(args)
+        Shellwords.shelljoin(args)
+      end
+
+      def cmd_opts
+        "-c #{config.conf_repo}"
+      end
     end
 
     Lita.register_handler(Pulsar)
--- spec/lita/handlers/pulsar_spec.rb
+++ spec/lita/handlers/pulsar_spec.rb
@@ -1,4 +1,24 @@
 require "spec_helper"
 
 describe Lita::Handlers::Pulsar, lita_handler: true do
+  it { is_expected.to route_command('deploy app stage').to(:deploy) }
+
+  it 'sets the conf_repo to nil by default' do
+    expect(registry.config.handlers.pulsar.conf_repo).to be_nil
+  end
+
+  context 'when :pulsar_conf configuration is specified' do
+    before do
+      registry.config.handlers.pulsar.conf_repo = 'git://pulsar_conf_repo'
+
+      allow(subject).to receive(:spawn)
+    end
+
+    it 'replies properly and calls pulsar' do
+      send_command('deploy app stage')
+
+      is_expected.to have_received(:spawn).with('eval `ssh-agent -s` && ssh-add && bundle exec pulsar -c git://pulsar_conf_repo app stage; kill $SSH_AGENT_PID')
+      expect(replies.last).to eq('OK, I will run a deploy soon...')
+    end
+  end
 end
